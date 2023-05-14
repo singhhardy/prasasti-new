b3f4ad8ae17cc47899bb36e42bfa65a889a2ed47
unknown <singhhardy724@gmail.comcleargit config --global user.name Hardik>
2023-05-14 12:36:17 +0530

    initial

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -6,11 +6,11 @@ function Showcase() {
       <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 p-heading text-start">Digital Awards, Badges and 
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 p-heading">Digital Awards, Badges and 
         <br class="hidden lg:inline-block" />Certificates on Blockchain
       </h1>
       <p class="mb-8 leading-relaxed">Store your Students, Employees, Participants <br/> Achievements on Blockchain and Built Trust.</p>
-      <div class="flex justify-center">
+      <div class="flex justify-center show-section">
       <input className="show-input outline-none bottom-shadow" placeholder="Enter Your Email" />
         <button class="primary-btn show-btn bottom-shadow">Get Started for free</button>
       </div>
